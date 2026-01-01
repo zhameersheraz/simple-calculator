a6f33fcbb575957cabb5b5a42ddb9faedb5cd603
Zhameer Sheraz Tampugao <zhameersheraztampugao@gmail.com>
2026-01-01 23:36:52 +0800

    first commit

---
+++ Calculator.java
@@ -0,0 +1,31 @@
+public class Calculator {
+    
+    public int add(int a, int b) {
+        return a + b;
+    }
+    
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+    
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+    
+    public double divide(int a, int b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("Cannot divide by zero");
+        }
+        return (double) a / b;
+    }
+    
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        
+        System.out.println("Simple Calculator - 2026");
+        System.out.println("5 + 3 = " + calc.add(5, 3));
+        System.out.println("5 - 3 = " + calc.subtract(5, 3));
+        System.out.println("5 × 3 = " + calc.multiply(5, 3));
+        System.out.println("5 ÷ 3 = " + calc.divide(5, 3));
+    }
+}
---
+++ README.md
@@ -0,0 +1,18 @@
+# Simple Calculator
+
+A basic calculator built with Java in 2026.
+
+## Features
+- Addition
+- Subtraction
+- Multiplication
+- Division
+
+## How to Run
+```bash
+javac Calculator.java
+java Calculator
+```
+
+## Author
+Built as my first GitHub project!
